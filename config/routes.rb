06734c9fa41d165ee905a68cbb47b63159855803
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-02-05 05:42:07 +0000

    [update]admin/userモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+  
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

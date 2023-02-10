bf319d03db6bf6e3a591b30a7e7910fcfc5b4328
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-02-10 23:47:57 +0000

    [update]ヘッダー修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,39 +16,19 @@ devise_for :admins, skip: [:registrations, :passwords], controllers: {
 
 namespace :user do
   root to: 'homes#index'
+  resources :users, only:[:show,:edit,:update,:index]
+  get '/users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+  patch '/users/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+  resources :comments, only:[:destroy,:create]
+  resources :likes, only:[:index,:create,:destroy,:show]
+  resources :post_images, only:[:index,:edit,:new,:create,:show]
 end
 
 namespace :admin do
   root to: 'homes#top'
   get "destroy/admin/session" => "admin#sign_out"
-end
-
-namespace :admin do
   resources :users, only:[:show,:edit,:update,:index]
-end
-
-namespace :admin do
   resources :post_images, only:[:edit,:destroy,:show]
-end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-namespace :user do
-  resources :users, only:[:show,:edit,:update,:index]
-  get '/users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
-  patch '/users/withdrawal' => 'users#withdrawal', as: 'withdrawal'
-end
-
-namespace :user do
-  resources :comments, only:[:destroy,:create]
-end
-
-namespace :user do
-  resources :likes, only:[:index,:create,:destroy,:show]
-end
-
-namespace :user do
-  resources :post_images, only:[:index,:edit,:new,:create,:show]
 end
 
 end

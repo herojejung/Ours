422cd6c7d67eb64cab300b0ea52f0d85b468d965
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-02-15 08:23:15 +0000

    [add]投稿詳細

--- app/controllers/user/post_images_controller.rb
+++ app/controllers/user/post_images_controller.rb
@@ -7,11 +7,12 @@ before_action :authenticate_user!
 
   def create
     @post_image = PostImage.new(post_image_params)
-    @post_image.save ? (redirect_to user_root_path(@post_image)) : (render :new)
+    @post_image.save ? (redirect_to user_post_image_path(@post_image)) : (render :new)
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @user = current_user
   end
   
   def edit
@@ -26,7 +27,7 @@ before_action :authenticate_user!
 private
   # ストロングパラメータ
 def post_image_params
-  params.require(:post_images).permit(:title,:text,:image,:latitude,:longitude,:user_id,:tag_id,:name)
+  params.require(:post_images).permit(:title,:text,:latitude,:longitude,:user_id,:tag_id,:name,image: [])
 end
 
 

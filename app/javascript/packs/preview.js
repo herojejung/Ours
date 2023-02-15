1a5f46c02464342cb26d9c8d13b42fe1070952f4
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-02-15 12:38:50 +0000

    [update]プレビュー画像複数投稿機能修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,34 +1,22 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+document.addEventListener('DOMContentLoaded', () => {
+  const createImageHTML = (blob) => {
+    const imageElement = document.getElementById('new-image');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('class', 'new-img')
+    blobImage.setAttribute('src', blob);
 
-require("@rails/ujs").start()
-// require("turbolinks").start()  // コメントアウト
-require("@rails/activestorage").start()
-require("channels")
-require('./preview')  // 追記
+    imageElement.appendChild(blobImage);
+  };
 
-if (document.URL.match(/new/)){
-  document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
-      const imageElement = document.getElementById('new-image');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'new-img')
-      blobImage.setAttribute('src', blob);
+  document.getElementById('post_image_images').addEventListener('change', (e) => {
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
 
-      imageElement.appendChild(blobImage);
-    };
-
-    document.getElementById('post_image_image').addEventListener('change', (e) => {
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+    Array.prototype.forEach.call(e.target.files, file => {
+      var blob = window.URL.createObjectURL(file);
       createImageHTML(blob);
     });
   });
-}
+});

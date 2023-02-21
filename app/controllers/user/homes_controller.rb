141fcad8c6d69cdc6bd0e98460c07c115d2f6726
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-02-21 11:40:49 +0000

    [update]トップページ修正

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,6 +1,6 @@
 class User::HomesController < ApplicationController
 def top
-  @post_images = PostImage.where(user_id: params[:user_id], post_image_id: params[:post_image_id]).order(created_at: :desc)
+  @post_images = PostImage.all.order(created_at: :desc)
 end
 
 end

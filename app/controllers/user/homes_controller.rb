57ae7ad806f5ea8e9eb1fcfc1e39837c1f0c62f6
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-03-17 23:54:35 +0000

    [update]投稿ランキング追加

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -2,16 +2,17 @@ class User::HomesController < ApplicationController
   def top
     @q = PostImage.ransack(params[:q])
     if params[:tag].present?
-      @post_images = PostImage.tagged_with(params[:tag]).order(created_at: :desc).page(params[:page]).per(5)
+      @post_images = PostImage.tagged_with(params[:tag]).order(created_at: :desc).page(params[:page]).per(4)
     elsif params[:sub_category_id].present?
-      @post_images = SubCategory.find(params[:sub_category_id]).post_images.order(created_at: :desc).page(params[:page]).per(5)
+      @post_images = SubCategory.find(params[:sub_category_id]).post_images.order(created_at: :desc).page(params[:page]).per(4)
     elsif params[:q].present?
-      @post_images = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(5)
+      @post_images = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(4)
     else
-      @post_images = PostImage.all.order(created_at: :desc).page(params[:page]).per(5)
+      @post_images = PostImage.all.order(created_at: :desc).page(params[:page]).per(4)
     end
     @tags = PostImage.tag_counts_on(:tags).order('count DESC').limit(10)
     @tags_ranking = PostImage.tag_counts_on(:tags).order('count DESC').limit(10)
+    @likes_ranking = PostImage.joins(:likes).group('post_images.id').having('COUNT(likes.id) > 0').order('COUNT(likes.id) DESC').limit(10)
   end
 
   private

b867d1ff5a1e4a0b693b4e2d3617af3052dfa9e6
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-03-25 05:36:25 +0000

    [update]カテゴリー名修正integer

--- db/migrate/20230308231554_create_subcategories.rb
+++ db/migrate/20230308231554_create_subcategories.rb
@@ -2,7 +2,7 @@ class CreateSubcategories < ActiveRecord::Migration[6.1]
   def change
     create_table :subcategories do |t|
       t.string :name
-      t.references :category, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
 
       t.timestamps
     end

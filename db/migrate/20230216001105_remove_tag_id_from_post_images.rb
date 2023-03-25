d4be7f686eb610fcf01f0d33078ca3a5e1e33f33
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-03-25 05:11:01 +0000

    [remove]tag_id

--- db/migrate/20230216001105_remove_tag_id_from_post_images.rb
+++
@@ -1,7 +0,0 @@
-class RemoveTagIdFromPostImages < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :post_images, :tag_id, :integer
-    remove_column :post_images, :tag_id, :integer
-    remove_column :post_images, :tag_id, :integer
-  end
-end

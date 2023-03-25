24d1cea0ec22c5c913ec78827a449c059abc8751
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-03-25 05:43:33 +0000

    [update]カテゴリーID,サブカテゴリーID/データ型bigint変更

--- db/migrate/20230308231554_create_subcategories.rb
+++ db/migrate/20230308231554_create_subcategories.rb
@@ -2,7 +2,7 @@ class CreateSubcategories < ActiveRecord::Migration[6.1]
   def change
     create_table :subcategories do |t|
       t.string :name
-      t.integer :category_id, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230314011052_add_category_id_and_subcategory_id_to_post_images.rb
+++ db/migrate/20230314011052_add_category_id_and_subcategory_id_to_post_images.rb
@@ -1,7 +1,7 @@
 class AddCategoryIdAndSubcategoryIdToPostImages < ActiveRecord::Migration[6.1]
   def change
-    add_column :post_images, :category_id, :integer
-    add_column :post_images, :sub_category_id, :integer
+    add_column :post_images, :category_id, :bigint
+    add_column :post_images, :sub_category_id, :bigint
     add_foreign_key :post_images, :categories, column: :category_id
     add_foreign_key :post_images, :sub_categories, column: :sub_category_id
   end

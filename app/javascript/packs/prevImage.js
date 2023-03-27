c1a6ec3c40800d2e8e518e27056af4d232ea3317
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-03-27 23:12:28 +0000

    [update]投稿詳細画面修正

--- app/javascript/packs/prevImage.js
+++ app/javascript/packs/prevImage.js
@@ -1,62 +1,12 @@
-/*global images $*/
-// 1. 画像要素を取得する。
-const images = document.getElementsByClassName('image-wrapper')[0].getElementsByTagName('img');
-// 2. 画像要素を配列に格納する。
-const imageArray = Array.from(images);
-// 3. 画像を表示するための変数を用意する。
-let currentIndex = 0;
-// 4. 次の画像を表示するための関数を定義する。
-function nextImage() {
-  currentIndex++;
-  if (currentIndex >= imageArray.length) {
-    currentIndex = 0;
-  }
-  showImage(currentIndex);
-}
-// 5. 前の画像を表示するための関数を定義する。
-function prevImage() {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = imageArray.length - 1;
-  }
-  showImage(currentIndex);
-}
-// 6. 次の画像を表示するためのイベントリスナーを追加する。
-document.querySelector('.prev-btn').addEventListener('click', () => {
-  nextImage();
-  if (currentIndex === 0) {
-    showImage(currentIndex);
-  }
-});
-// 7. 前の画像を表示するためのイベントリスナーを追加する。
-document.querySelector('.next-btn').addEventListener('click', () => {
-  prevImage();
-  if (currentIndex === 0) {
-    showImage(currentIndex);
-  }
-});
-// 画像を表示する関数
-function showImage(index) {
-  imageArray.forEach((image, i) => {
-    if (i === index) {
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
+ /*global $*/
+$('.slick-slider').slick({
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1
+      }
     }
-  });
-}
-// すべての画像を非表示にする
-showImage(currentIndex);
-// currentIndexを0に設定する
-currentIndex = 0;
-// リロード時にcurrentIndexを0にリセットする
-window.addEventListener('load', () => {
-  currentIndex = 0;
+  ]
 });
-// 最初の画像を表示する
-if (imageArray.length === 1) {
-  document.querySelector('.prev-btn').style.display = 'none';
-  document.querySelector('.next-btn').style.display = 'none';
-} else {
-  showImage(currentIndex);
-}

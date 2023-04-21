bd1cb61ed45dc2484b45204332ad2ea2d2510a21
EC2 Default User <114767198+herojejung@users.noreply.github.com>
2023-04-21 11:28:57 +0000

    [update]ajaxいいね機能修正

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -1,20 +1,40 @@
 class User::LikesController < ApplicationController
+  before_action :set_post_image
+  before_action :check_user_liked, only: :create
+  before_action :check_user_unliked, only: :destroy
+
   def index
   end
 
   def create
     @post_image = PostImage.find(params[:post_image_id])
     current_user.likes.create(post_image: @post_image)
-    redirect_back(fallback_location: root_path)
+     respond_to do |format|
+    format.js { render :create }
+  end
   end
 
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
     current_user.likes.find_by(post_image: @post_image).destroy
-    redirect_back(fallback_location: root_path)
+     respond_to do |format|
+    format.js { render :destroy }
+  end
   end
 
 
-  def show
+private
+
+  def set_post_image
+    @post_image = PostImage.find(params[:post_image_id])
   end
+
+  def check_user_liked
+    redirect_to root_path if @post_image.likes.exists?(user_id: current_user.id)
+  end
+
+  def check_user_unliked
+    redirect_to root_path unless @post_image.likes.exists?(user_id: current_user.id)
+  end
+
 end
